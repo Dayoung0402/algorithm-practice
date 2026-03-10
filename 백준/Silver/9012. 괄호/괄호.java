f8bf404d71fb8072b0cd18ce504d6f8444b538e2
다영 <pdayoung0402@hufs.ac.kr>
2026-03-11 05:57:13 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -6,32 +6,29 @@ public class Main {
     public static void main(String args[]) throws Exception {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine()); // 몇번 돌건지
-        
+        int num = Integer.parseInt(br.readLine()); // 몊번의 test인지
+             
         for(int i = 0; i < num; i++) {
+            Deque<Integer> stack = new ArrayDeque<>();
+            boolean valid = true;
             String line = br.readLine();
-            int length = line.length(); // 받은 문자열의 길이 확보
-            Deque<Integer> stack = new ArrayDeque<>(); // 스택 선언
-            boolean valid = true; // 스택이 차있는지(true), 비어있는지(false)
             
-            for(int j = 0; j < length; j++) {
-                if(line.charAt(j) == '(') {
-                    stack.push(1);
-                }
+            for(int j = 0; j < line.length(); j++) {
+                if(line.charAt(j) == '(') stack.push(1);
+                else if (!stack.isEmpty() && line.charAt(j) == ')') {
+                    stack.pop();
+                    }
                 else {
-                    if(stack.isEmpty()) {
-                        valid = false;
-                        break;
-                    } else stack.pop();
+                    valid = false;
+                    break;
                 }
-            }
-            
-            if(valid && stack.isEmpty()){
+             }
+      
+            if(valid && stack.isEmpty()) {
                 System.out.println("YES");
-            } else System.out.println("NO");
-            
-            
+            } else {
+                System.out.println("NO");
+            }
         }
-        
     }
 }
