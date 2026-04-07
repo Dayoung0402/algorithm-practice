dc573f21c0116536d8d2d5b98b33e0af3b6e1876
다영 <pdayoung0402@hufs.ac.kr>
2026-04-07 22:22:49 +0900

    [Bronze II] Title: 단어의 개수, Time: 308 ms, Memory: 41400 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int result = 0;
+        
+        while(st.hasMoreTokens()) {
+            st.nextToken();
+            result++;
+        }
+        
+        System.out.println(result);
+    }
+    
+}
