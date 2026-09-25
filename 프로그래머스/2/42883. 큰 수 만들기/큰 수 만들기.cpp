5bfb6c288cad0c0717027b3c19f645913ee403e2
다영 <pdayoung0402@hufs.ac.kr>
2026-09-26 01:37:06 +0900

    [level 2] Title: 큰 수 만들기, Time: 3.43 ms, Memory: 5.95 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -11,31 +11,35 @@ string solution(string number, int k) {
     cin.tie(NULL);
     
     string answer = "";
-    
-    // 스택 이용 : 마주한 숫자가 담아 놓은 숫자보다 크면 담아 놓은 숫자를 버린다.
+    // 전체 흐름 정리
+    /*
+    1. 인덱스 0을 일단 스택에 넣고
+    2. 인덱스 1부터는 앞서 스택 값들(바로 앞 값 뿐만 아니라 이전도)과 비교해서 
+    -> 작거나 같으면 그냥 같다 붙이고
+    -> 크면 기존 스택 값 빼고 갖다 붙임
+    */
     stack<char> s;
-    s.push(number[0]); // 일단 첫번째 1 넣고
+    s.push(number[0]);
+    int len = number.size();
     int chance = k;
     
-    for(int i = 1; i < number.size(); i++) {
-            
-        while(!s.empty() && chance > 0 && s.top() < number[i]) { // 9가 1보다 크니깐 9를 스택에 집어넣고 chance 하나 줄이고
+    for(int i = 1; i < len; i++) {
+        while(!s.empty() && chance > 0 && s.top() < number[i]) {
             s.pop();
             chance--;
         }
         s.push(number[i]);
     }
     
-    while(chance > 0) {
+    while(chance > 0) { //다 돌았는데도 뺄게 없으면 걍 뒤에꺼 빼고
         s.pop();
         chance--;
     }
     
-    int size_s = s.size();
-    for(int i = 0; i < size_s; i++) {
-            answer += (s.top());
-            s.pop();
-        }
+    while(!s.empty()) {
+        answer += s.top();
+        s.pop();
+    }
     
     reverse(answer.begin(), answer.end());
     
