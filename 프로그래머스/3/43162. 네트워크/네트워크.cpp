de7754c5c3137a93774b884e79626735e1136cf3
다영 <pdayoung0402@hufs.ac.kr>
2026-09-26 23:52:11 +0900

    [level 3] Title: 네트워크, Time: 0.07 ms, Memory: 4.79 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -4,22 +4,18 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-vector<int> graph[200+1]; // 여기서는 graph[4];
+vector<int> graph[200+1]; 
 bool visited[200+1];
 int answer = 0;
 
 void dfs(int cur) {
     
-    visited[cur] = true;
-    
-    for(int next : graph[cur]) {
-        if(!visited[next]) {
+    for(int next : graph[cur]){
+        if(!visited[next]){
             visited[next] = true;
             dfs(next);
         }
     }
-    
 }
 
 int solution(int n, vector<vector<int>> computers) {
@@ -27,22 +23,21 @@ int solution(int n, vector<vector<int>> computers) {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    // computer 연결 그래프로 표현
-    
-    for(int i = 0; i < n; i++) {
+   //일단 그래프에 집어넣어서 보기 좋게
+    for(int i = 0; i< n; i++) {
         for(int j = i+1; j < n; j++) {
-            if(computers[i][j] ==1) {
+            if(computers[i][j] == 1) {
                 graph[i].push_back(j);
-                graph[j].push_back(i);                
-            }
+                graph[j].push_back(i);
+            }            
         }
-    } //무 방향 그래프니깐 양쪽으로 집어넣고
+    }
     
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i< n; i++) {
         if(!visited[i]) {
             answer++;
-            dfs(i);    
-        }    
+            dfs(i);
+        }
     }
     
     
